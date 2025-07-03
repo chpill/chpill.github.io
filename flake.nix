f3e708ac9b2f72cede8f39ba60830df1904af087
chpill <etienne@spillemaeker.com>
2025-07-03 16:16:41 +0200

    Pin nixpkgs revision via a proxy flake

--- flake.nix
+++ flake.nix
@@ -1,18 +1,21 @@
 {
   description = "Technical blog";
-  inputs.nixpkgs-unstable.url = "nixpkgs-unstable";
-  outputs = { self, nixpkgs-unstable }@inputs:
+  inputs = {
+    proxy-flake.url = "github:chpill/proxy-flake";
+    nixpkgs.follows = "proxy-flake/nixpkgs";
+  };
+  outputs = { self, nixpkgs , ... }@inputs:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs-unstable.legacyPackages.${system};
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
           pandoc
-          (clojure.override { jdk = jdk23; })
+          (clojure.override { jdk = jdk24; })
           # To view the pages locally:
-          # cd publish && httplz
-          httplz
+          # cd publish && python -m http.server
+          python3
           # To check that the feed is valid xml
           # xmllint --noout **/*.xml
           libxml2
